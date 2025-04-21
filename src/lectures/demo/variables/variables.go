0a77646a0770342bf2ea9ab0368da38c7ae91af9
olatunji-weber <olatunji.weber@gmail.com>
2025-04-21 23:42:39 +0200

    Variables Syntax...

--- src/lectures/demo/variables/variables.go
+++ src/lectures/demo/variables/variables.go
@@ -3,5 +3,13 @@ package main
 import "fmt"
 
 func main() {
-}
+	var myName, age = "Olayinka", 45
+	fmt.Println("My name is: ", myName, ", I am ", age, " years old. Thank you.")
 
+	var name string = "Toluwalope"
+	fmt.Println("His wife's name is: ", name)
+
+	userName := "YinkaVitula"
+	fmt.Println("User name is ", userName)
+
+}
