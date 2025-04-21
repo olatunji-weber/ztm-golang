e54d6a6ead687ea5c9881ac75994587800415259
olatunji-weber <olatunji.weber@gmail.com>
2025-04-22 00:02:37 +0200

    more on variables..

--- src/lectures/demo/variables/variables.go
+++ src/lectures/demo/variables/variables.go
@@ -12,4 +12,19 @@ func main() {
 	userName := "YinkaVitula"
 	fmt.Println("User name is ", userName)
 
+	var sum int
+	fmt.Println("The sum is: ", sum)
+
+	// number1, otherInfo := 50, "random info"
+	// fmt.Println("The first number is: ", number1, "and otherinfo is: ", otherInfo)
+
+	// number2, otherInfo := 100, "some other info***"
+	// fmt.Println("The second number is: ", number2, "and otherinfo is: ", otherInfo)
+	var (
+		number1 = 500
+		number2 = 600
+	)
+
+	sum = number1 + number2
+	fmt.Println("The sum of the number is: ", sum)
 }
