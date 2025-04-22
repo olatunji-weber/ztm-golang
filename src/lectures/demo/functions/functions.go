8c47e8271297ee69c1115de5827dbbc11e466131
olatunji-weber <olatunji.weber@gmail.com>
2025-04-22 02:57:06 +0200

    Learning about functions

--- src/lectures/demo/functions/functions.go
+++ src/lectures/demo/functions/functions.go
@@ -2,7 +2,26 @@ package main
 
 import "fmt"
 
-func main() {
-
+func double(number int) int {
+	return number + number
 }
 
+func add(lhs, rhs int) int {
+	return lhs + rhs
+}
+
+func greet() {
+	fmt.Println("Hello from Greet Function!")
+}
+
+func main() {
+	greet()
+	dozen := double(6)
+	fmt.Println("A Dozen is: ", dozen)
+
+	bakersDozen := add(dozen, 1)
+	fmt.Println("A baker's dozen is: ", bakersDozen)
+
+	anotherBakersDozen := add(double(6), 1)
+	fmt.Println("Another baker's dozen is: ", anotherBakersDozen)
+}
--- src/lectures/exercise/functions/functions.go
+++ src/lectures/exercise/functions/functions.go
@@ -1,25 +1,51 @@
-//--Summary:
-//  Use functions to perform basic operations and print some
-//  information to the terminal.
+// --Summary:
 //
-//--Requirements:
-//* Write a function that accepts a person's name as a function
-//  parameter and displays a greeting to that person.
-//* Write a function that returns any message, and call it from within
-//  fmt.Println()
-//* Write a function to add 3 numbers together, supplied as arguments, and
-//  return the answer
-//* Write a function that returns any number
-//* Write a function that returns any two numbers
-//* Add three numbers together using any combination of the existing functions.
-//  * Print the result
-//* Call every function at least once
-
+//	Use functions to perform basic operations and print some
+//	information to the terminal
 package main
 
-import "fmt"
+import (
+	"fmt"
+)
 
-func main() {
-
+// --Requirements:
+// * Write a function that accepts a person's name as a function parameter and displays a greeting to that person.
+func greeting(name string) {
+	fmt.Println("Hello there,", name, "it's nice to meet you.")
 }
 
+//   - Write a function that returns any message, and call it from within
+//     fmt.Println()
+func messageEcho(message string) string {
+	return message
+}
+
+//   - Write a function to add 3 numbers together, supplied as arguments, and
+//     return the answer
+func addNumbers(num1, num2, num3 int) int {
+	return num1 + num2 + num3
+}
+
+// - Write a function that returns any number
+func anyNumber(num int) int {
+	return num
+}
+
+// - Write a function that returns any two numbers
+// - Add three numbers together using any combination of the existing functions.
+// - Print the result
+func main() {
+	//* Call every function at least once
+	var username string
+	fmt.Println("What is your name? ")
+	fmt.Scanln(&username)
+	greeting(username)
+
+	myMessage := "This is my message to you"
+	fmt.Println(messageEcho(myMessage))
+
+	a, b, c := 500, 100, 400
+	fmt.Println("The sum of the 3 numbers is: ", addNumbers(a, b, c))
+
+	fmt.Println("The number to be returned is: ", anyNumber(20))
+}
